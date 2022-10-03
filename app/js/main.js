26381b3373adbf9dc22d6adcad54b855e05779d7
polvad <polvad2308@gmail.com>
2022-10-03 20:53:20 +0300

    add menu-popup

--- app/js/main.js
+++ app/js/main.js
@@ -47,3 +47,32 @@ const swiper = new Swiper('.new__container', {
 
 });
 
+let burgerBtn = document.querySelector('.burger');
+let menuPopup = document.querySelector('.header__popup');
+let closePopup = document.querySelector('.popup__close')
+
+burgerBtn.addEventListener("click", function() {
+  menuPopup.classList.add("popup-show");
+});
+
+closePopup.addEventListener("click", function () {
+  menuPopup.classList.remove("popup-show");
+});
+
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
